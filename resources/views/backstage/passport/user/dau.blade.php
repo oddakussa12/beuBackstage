aa1dd9e49c7aad285e242e3d76039977e738adf0
2236559110@qq.com <2236559110@qq.com>
2021-02-19 13:23:56 +0800

    test

--- resources/views/backstage/passport/user/dau.blade.php
+++ resources/views/backstage/passport/user/dau.blade.php
@@ -38,25 +38,7 @@
                 <th>>3NUM%</th>
             </tr>
             </thead>
-            @foreach($list as $l)
-                <tr>
-                    <td>{{$l['date']}}</td>
-{{--                    <td>{{$l['dau']}}</td>--}}
-{{--                    <td>{{$l['zero']}}</td>--}}
-{{--                    @if ($l['dau']==0)--}}
-{{--                    <td>0%</td>--}}
-{{--                    <td>0%</td>--}}
-{{--                    <td>0%</td>--}}
-{{--                    <td>0%</td>--}}
-{{--                    @else--}}
-{{--                    <td>{{round($l['zero']/$l['dau']*100 , 2)}}%</td>--}}
-{{--                    <td>{{round($l['one']/$l['dau']*100 , 2)}}%</td>--}}
-{{--                    <td>{{round($l['two']/$l['dau']*100 , 2)}}%</td>--}}
-{{--                    <td>{{round($l['gt3']/$l['dau']*100 , 2)}}%</td>--}}
-{{--                    @endif--}}
-                </tr>
-            @endforeach
-            </tbody>
+
         </table>
     </div>
 @endsection
