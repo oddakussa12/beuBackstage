d098fedc6ebbee1d9d4eca8fbb401225a7d84fc4
2236559110@qq.com <2236559110@qq.com>
2021-02-19 14:00:31 +0800

    test

--- resources/views/backstage/passport/user/dau.blade.php
+++ resources/views/backstage/passport/user/dau.blade.php
@@ -1,79 +1,94 @@
 @extends('layouts.dashboard')
 @section('layui-content')
     <div  class="layui-fluid">
-        <form class="layui-form" action="" lay-filter="keep">
-            <div class="layui-form-item">
-                <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
-                <div class="layui-inline">
-                    <select name="country_code" lay-verify="" lay-search>
-                        <option value="">{{trans('user.form.placeholder.user_country')}}</option>
-                        @foreach($counties  as $country)
-                            <option value="{{strtolower($country['code'])}}" @if($country_code==strtolower($country['code'])) selected @endif>{{$country['name']}}</option>
+        <div class="layui-tab">
+            <ul class="layui-tab-title">
+                <li class="layui-this">DAU</li>
+                <li>DAU</li>
+            </ul>
+            <div class="layui-tab-content">
+                <div class="layui-tab-item layui-show">
+                    <form class="layui-form" action="" lay-filter="keep">
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
+                            <div class="layui-inline">
+                                <select name="country_code" lay-verify="" lay-search>
+                                    <option value="">{{trans('user.form.placeholder.user_country')}}</option>
+                                    @foreach($counties  as $country)
+                                        <option value="{{strtolower($country['code'])}}" @if($country_code==strtolower($country['code'])) selected @endif>{{$country['name']}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">{{trans('user.form.label.date')}}:</label>
+                            <div class="layui-input-inline">
+                                <input type="text" class="layui-input" name="period" id="period" placeholder="yyyy-MM-dd - yyyy-MM-dd" value="{{$period}}">
+                            </div>
+                            <div class="layui-inline">
+                                <button class="layui-btn" type="submit"  lay-submit >{{trans('common.form.button.submit')}}</button>
+                            </div>
+                        </div>
+                    </form>
+                    <table class="layui-table" >
+                        <thead>
+                        <tr>
+                            <th>Day</th>
+                            <th>DUA</th>
+                            <th>0NUM</th>
+                            <th>0NUM%</th>
+                            <th>1NUM</th>
+                            <th>1NUM%</th>
+                            <th>2NUM</th>
+                            <th>2NUM%</th>
+                            <th>>3NUM</th>
+                            <th>>3NUM%</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($list as $l)
+                            <tr>
+                                <td>{{$l['date']}}</td>
+                                <td>{{$l['dau']}}</td>
+                                <td>{{$l['zero']}}</td>
+                                @if ($l['dau']==0)
+                                    <td>0%</td>
+                                @else
+                                    <td>{{round($l['zero']/$l['dau']*100 , 2)}}%</td>
+                                @endif
+
+                                <td>{{$l['one']}}</td>
+                                @if ($l['dau']==0)
+                                    <td>0%</td>
+                                @else
+                                    <td>{{round($l['one']/$l['dau']*100 , 2)}}%</td>
+                                @endif
+
+                                <td>{{$l['two']}}</td>
+                                @if ($l['dau']==0)
+                                    <td>0%</td>
+                                @else
+                                    <td>{{round($l['two']/$l['dau']*100 , 2)}}%</td>
+                                @endif
+
+                                <td>{{$l['gt3']}}</td>
+                                @if ($l['dau']==0)
+                                    <td>0%</td>
+                                @else
+                                    <td>{{round($l['gt3']/$l['dau']*100 , 2)}}%</td>
+                                @endif
+                            </tr>
                         @endforeach
-                    </select>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="layui-tab-item">
+                    <div id="container" style="height: 100%"></div>
                 </div>
             </div>
-            <div class="layui-form-item">
-                <label class="layui-form-label">{{trans('user.form.label.date')}}:</label>
-                <div class="layui-input-inline">
-                    <input type="text" class="layui-input" name="period" id="period" placeholder="yyyy-MM-dd - yyyy-MM-dd" value="{{$period}}">
-                </div>
-                <div class="layui-inline">
-                    <button class="layui-btn" type="submit"  lay-submit >{{trans('common.form.button.submit')}}</button>
-                </div>
-            </div>
-        </form>
-        <table class="layui-table" >
-            <thead>
-            <tr>
-                <th>Day</th>
-                <th>DUA</th>
-                <th>0NUM</th>
-                <th>0NUM%</th>
-                <th>1NUM</th>
-                <th>1NUM%</th>
-                <th>2NUM</th>
-                <th>2NUM%</th>
-                <th>>3NUM</th>
-                <th>>3NUM%</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($list as $l)
-                <tr>
-                    <td>{{$l['date']}}</td>
-                    <td>{{$l['dau']}}</td>
-                    <td>{{$l['zero']}}</td>
-                    @if ($l['dau']==0)
-                    <td>0%</td>
-                    @else
-                    <td>{{round($l['zero']/$l['dau']*100 , 2)}}%</td>
-                    @endif
+        </div>
 
-                    <td>{{$l['one']}}</td>
-                    @if ($l['dau']==0)
-                        <td>0%</td>
-                    @else
-                        <td>{{round($l['one']/$l['dau']*100 , 2)}}%</td>
-                    @endif
 
-                    <td>{{$l['two']}}</td>
-                    @if ($l['dau']==0)
-                        <td>0%</td>
-                    @else
-                        <td>{{round($l['two']/$l['dau']*100 , 2)}}%</td>
-                    @endif
-
-                    <td>{{$l['gt3']}}</td>
-                    @if ($l['dau']==0)
-                        <td>0%</td>
-                    @else
-                        <td>{{round($l['gt3']/$l['dau']*100 , 2)}}%</td>
-                    @endif
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
 @endsection
 @section('footerScripts')
@@ -90,13 +105,15 @@
             base: "{{url('plugin/layui')}}/"
         }).extend({
             common: 'lay/modules/admin/common',
-        }).use(['common' , 'table' , 'layer' , 'flow' , 'laydate'], function () {
+            echarts: 'lay/modules/echarts',
+        }).use(['common' , 'table' , 'layer' , 'flow' , 'laydate' , 'echarts'], function () {
             var $ = layui.jquery,
                 table = layui.table,
                 form = layui.form,
                 common = layui.common,
                 layer = layui.layer,
                 flow = layui.flow,
+                echarts = layui.echarts,
                 laydate = layui.laydate;
 
             laydate.render({
@@ -112,6 +129,77 @@
             table.init('user_table', { //转化静态表格
                 page:false
             });
+
+            var dom = document.getElementById("container");
+            var myChart = echarts.init(dom);
+            var app = {};
+            option = null;
+            option = {
+                title: {
+                    text: '折线图堆叠'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '邮件营销',
+                        type: 'line',
+                        stack: '总量',
+                        data: [120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name: '联盟广告',
+                        type: 'line',
+                        stack: '总量',
+                        data: [220, 182, 191, 234, 290, 330, 310]
+                    },
+                    {
+                        name: '视频广告',
+                        type: 'line',
+                        stack: '总量',
+                        data: [150, 232, 201, 154, 190, 330, 410]
+                    },
+                    {
+                        name: '直接访问',
+                        type: 'line',
+                        stack: '总量',
+                        data: [320, 332, 301, 334, 390, 330, 320]
+                    },
+                    {
+                        name: '搜索引擎',
+                        type: 'line',
+                        stack: '总量',
+                        data: [820, 932, 901, 934, 1290, 1330, 1320]
+                    }
+                ]
+            };
+            ;
+            if (option && typeof option === "object") {
+                myChart.setOption(option, true);
+            }
 
         })
     </script>
