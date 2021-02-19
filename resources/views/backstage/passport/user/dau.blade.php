cef6285a14fec6314fade4dbb7b093b6d9a525f5
2236559110@qq.com <2236559110@qq.com>
2021-02-19 15:02:04 +0800

    test

--- resources/views/backstage/passport/user/dau.blade.php
+++ resources/views/backstage/passport/user/dau.blade.php
@@ -15,9 +15,7 @@
     </div>
     <div class="layui-body">
         <!-- 内容主体区域 -->
-        <div class="layui-fluid">
             @include('layouts.bread_crumb')
-            <div  class="layui-fluid">
                 <div class="layui-tab">
                     <ul class="layui-tab-title">
                         <li>DAU</li>
@@ -25,90 +23,87 @@
                     </ul>
                     <div class="layui-tab-content">
                         <div class="layui-tab-item">
-                            <form class="layui-form" action="" lay-filter="keep">
-                                <div class="layui-form-item">
-                                    <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
-                                    <div class="layui-inline">
-                                        <select name="country_code" lay-verify="" lay-search>
-                                            <option value="">{{trans('user.form.placeholder.user_country')}}</option>
-                                            @foreach($counties  as $country)
-                                                <option value="{{strtolower($country['code'])}}" @if($country_code==strtolower($country['code'])) selected @endif>{{$country['name']}}</option>
-                                            @endforeach
-                                        </select>
+                            <div class="layui-fluid">
+                                <form class="layui-form" action="" lay-filter="keep">
+                                    <div class="layui-form-item">
+                                        <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
+                                        <div class="layui-inline">
+                                            <select name="country_code" lay-verify="" lay-search>
+                                                <option value="">{{trans('user.form.placeholder.user_country')}}</option>
+                                                @foreach($counties  as $country)
+                                                    <option value="{{strtolower($country['code'])}}" @if($country_code==strtolower($country['code'])) selected @endif>{{$country['name']}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="layui-form-item">
-                                    <label class="layui-form-label">{{trans('user.form.label.date')}}:</label>
-                                    <div class="layui-input-inline">
-                                        <input type="text" class="layui-input" name="period" id="period" placeholder="yyyy-MM-dd - yyyy-MM-dd" value="{{$period}}">
+                                    <div class="layui-form-item">
+                                        <label class="layui-form-label">{{trans('user.form.label.date')}}:</label>
+                                        <div class="layui-input-inline">
+                                            <input type="text" class="layui-input" name="period" id="period" placeholder="yyyy-MM-dd - yyyy-MM-dd" value="{{$period}}">
+                                        </div>
+                                        <div class="layui-inline">
+                                            <button class="layui-btn" type="submit"  lay-submit >{{trans('common.form.button.submit')}}</button>
+                                        </div>
                                     </div>
-                                    <div class="layui-inline">
-                                        <button class="layui-btn" type="submit"  lay-submit >{{trans('common.form.button.submit')}}</button>
-                                    </div>
-                                </div>
-                            </form>
-                            <table class="layui-table" >
-                                <thead>
-                                <tr>
-                                    <th>Day</th>
-                                    <th>DUA</th>
-                                    <th>0NUM</th>
-                                    <th>0NUM%</th>
-                                    <th>1NUM</th>
-                                    <th>1NUM%</th>
-                                    <th>2NUM</th>
-                                    <th>2NUM%</th>
-                                    <th>>3NUM</th>
-                                    <th>>3NUM%</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($list as $l)
+                                </form>
+                                <table class="layui-table" >
+                                    <thead>
                                     <tr>
-                                        <td>{{$l['date']}}</td>
-                                        <td>{{$l['dau']}}</td>
-                                        <td>{{$l['zero']}}</td>
-                                        @if ($l['dau']==0)
-                                            <td>0%</td>
-                                        @else
-                                            <td>{{round($l['zero']/$l['dau']*100 , 2)}}%</td>
-                                        @endif
-
-                                        <td>{{$l['one']}}</td>
-                                        @if ($l['dau']==0)
-                                            <td>0%</td>
-                                        @else
-                                            <td>{{round($l['one']/$l['dau']*100 , 2)}}%</td>
-                                        @endif
-
-                                        <td>{{$l['two']}}</td>
-                                        @if ($l['dau']==0)
-                                            <td>0%</td>
-                                        @else
-                                            <td>{{round($l['two']/$l['dau']*100 , 2)}}%</td>
-                                        @endif
-
-                                        <td>{{$l['gt3']}}</td>
-                                        @if ($l['dau']==0)
-                                            <td>0%</td>
-                                        @else
-                                            <td>{{round($l['gt3']/$l['dau']*100 , 2)}}%</td>
-                                        @endif
+                                        <th>Day</th>
+                                        <th>DUA</th>
+                                        <th>0NUM</th>
+                                        <th>0NUM%</th>
+                                        <th>1NUM</th>
+                                        <th>1NUM%</th>
+                                        <th>2NUM</th>
+                                        <th>2NUM%</th>
+                                        <th>>3NUM</th>
+                                        <th>>3NUM%</th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($list as $l)
+                                        <tr>
+                                            <td>{{$l['date']}}</td>
+                                            <td>{{$l['dau']}}</td>
+                                            <td>{{$l['zero']}}</td>
+                                            @if ($l['dau']==0)
+                                                <td>0%</td>
+                                            @else
+                                                <td>{{round($l['zero']/$l['dau']*100 , 2)}}%</td>
+                                            @endif
+
+                                            <td>{{$l['one']}}</td>
+                                            @if ($l['dau']==0)
+                                                <td>0%</td>
+                                            @else
+                                                <td>{{round($l['one']/$l['dau']*100 , 2)}}%</td>
+                                            @endif
+
+                                            <td>{{$l['two']}}</td>
+                                            @if ($l['dau']==0)
+                                                <td>0%</td>
+                                            @else
+                                                <td>{{round($l['two']/$l['dau']*100 , 2)}}%</td>
+                                            @endif
+
+                                            <td>{{$l['gt3']}}</td>
+                                            @if ($l['dau']==0)
+                                                <td>0%</td>
+                                            @else
+                                                <td>{{round($l['gt3']/$l['dau']*100 , 2)}}%</td>
+                                            @endif
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                         <div class="layui-tab-item  layui-show layui-echarts">
-
+                            <div id="container" style="height: 100%"></div>
                         </div>
                     </div>
                 </div>
-
-            </div>
-        </div>
-        <div id="container" style="height: 100%"></div>
-
     </div>
     <div class="layui-footer">
         <!-- 底部固定区域 -->
