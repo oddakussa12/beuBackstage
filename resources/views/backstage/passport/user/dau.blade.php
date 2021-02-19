d710704d338796b2a8f18a04552708cefb752f2d
2236559110@qq.com <2236559110@qq.com>
2021-02-19 16:34:19 +0800

    fix dau echarts

--- resources/views/backstage/passport/user/dau.blade.php
+++ resources/views/backstage/passport/user/dau.blade.php
@@ -250,9 +250,10 @@
             });
 
             element.on('tab(dau)', function(data){
-                console.log(this); //当前Tab标题所在的原始DOM元素
-                console.log(data.index); //得到当前Tab的所在下标
-                console.log(data.elem); //得到当前的Tab大容器
+                if(data.index==1)
+                {
+                    autoContainer();
+                }
             });
 
 
