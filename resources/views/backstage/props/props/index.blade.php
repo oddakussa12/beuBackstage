f1976282905b9f207a179d174af66a5ebd420c6c
tsmtang <t349359058@sina.com>
2021-03-09 16:11:31 +0800

    test

--- resources/views/backstage/props/props/index.blade.php
+++ resources/views/backstage/props/props/index.blade.php
@@ -27,7 +27,6 @@
                 <th lay-data="{field:'id', width:80 , fixed: 'left'}">ID</th>
                 <th lay-data="{field:'cover', width:80, fixed: 'left'}">Image</th>
                 <th lay-data="{field:'name', width:150,sort:true, fixed: 'left'}">Name</th>
-{{--                <th lay-data="{field:'type', width:100}">类型</th>--}}
                 <th lay-data="{field:'recommendation', width:120}">Recommend</th>
                 <th lay-data="{field:'category', width:100}">Category</th>
                 <th lay-data="{field:'camera', width:80}">Camera</th>
@@ -36,8 +35,6 @@
                 <th lay-data="{field:'is_delete', width:100}">Status</th>
                 <th lay-data="{field:'url', width:360}">URL</th>
                 <th lay-data="{field:'created_at', width:160}">Created_at</th>
-{{--                <th lay-data="{field:'updated_at', width:160}">Updated_at</th>--}}
-{{--                <th lay-data="{field:'deleted_at', width:160}">删除时间</th>--}}
                 <th lay-data="{fixed: 'right', minWidth:100, align:'center', toolbar: '#op'}">{{trans('common.table.header.op')}}</th>
 
             </tr>
