3b34aa178cb54df066a3f3195e328bd425b68ecf
2236559110@qq.com <2236559110@qq.com>
2021-03-15 18:52:45 +0800

    fix dnu

--- resources/views/backstage/passport/user/dnu.blade.php
+++ resources/views/backstage/passport/user/dnu.blade.php
@@ -26,7 +26,7 @@
                 <div class="layui-tab-item ">
                     <div class="layui-fluid">
                         <form class="layui-form" action="" lay-filter="keep">
-                            @if(Auth::user()->hasRole('admin'))
+                            @if(Auth::user()->hasRole('administrator'))
                             <div class="layui-form-item">
                                 <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
                                 <div class="layui-inline">
