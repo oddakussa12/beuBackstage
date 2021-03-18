f89ed785c85134aded4ed13fb126804af5fde966
2236559110@qq.com <2236559110@qq.com>
2021-03-18 20:12:03 +0800

    fix remove useless file

---
+++ app/Repositories/Eloquent/Content/UserRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+
+interface UserRepository extends BaseRepository
+{
+    public function isDeletedUser($name);
+
+    public function isBlackUser($user_id);
+
+}
