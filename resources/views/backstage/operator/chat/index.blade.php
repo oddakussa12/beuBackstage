25f4c36fdd2a22a6980c397fcd004bc060681473
2236559110@qq.com <2236559110@qq.com>
2021-03-19 14:33:36 +0800

    fix school

--- resources/views/backstage/operator/chat/index.blade.php
+++ resources/views/backstage/operator/chat/index.blade.php
@@ -18,10 +18,10 @@
                             </div>
 
                             <div class="layui-inline">
-                                <label class="layui-form-label">School:</label>
+                                <label class="layui-form-label">{{trans('user.form.label.user_school')}}:</label>
                                 <div class="layui-input-inline">
                                     <select  name="school" lay-verify="" lay-search  id="school">
-                                        <option value="">{{trans('user.form.placeholder.label.user_school')}}</option>
+                                        <option value="">{{trans('user.form.placeholder.user_school')}}</option>
                                         @foreach($schools  as $s)
                                             <option value="{{$s}}" @if(!empty($school)&&$school==$s) selected @endif>{{$s}}</option>
                                         @endforeach
@@ -30,10 +30,10 @@
                             </div>
 
                             <div class="layui-inline">
-                                <label class="layui-form-label">{{trans('comment.form.placeholder.comment_country_id')}}:</label>
+                                <label class="layui-form-label">{{trans('user.form.label.user_country')}}:</label>
                                 <div class="layui-input-inline">
                                     <select  name="country_code" lay-verify="" lay-search  >
-                                        <option value="">{{trans('comment.form.placeholder.comment_country_id')}}</option>
+                                        <option value="">{{trans('user.form.placeholder.user_country')}}</option>
                                         @foreach($countries  as $country)
                                             <option value="{{$country['code']}}" @if(!empty($country_code)&&$country_code==$country['code']) selected @endif>{{$country['name']}}</option>
                                         @endforeach
