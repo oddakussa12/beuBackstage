41a0c3e8b1361db6d60db2f23526b6ca48a9ad03
2236559110@qq.com <2236559110@qq.com>
2021-07-02 09:32:37 +0800

    fix order

--- resources/views/backstage/business/order/index.blade.php
+++ resources/views/backstage/business/order/index.blade.php
@@ -103,6 +103,7 @@
                 $ = layui.jquery;
             var order = table.init('table', { //转化静态表格
                 page:false,
+                toolbar: '#toolbar',
                 done: function(res, curr, count){
                     console.log(res.data);
                     $('th').css({'font-size': '15'});	//进行表头样式设置
